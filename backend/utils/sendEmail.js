4da45ca4d4383ae4bde2f3d6cac6d32259a852a1
Timi-cell <adedayotimilehin10@gmail.com>
2023-07-29 19:18:28 +0100

    revert change

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,7 +3,7 @@ const sendEmail = async (subject, msg, send_to, send_from, reply_to) => {
   // Create Email Transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465,
+    port: 587,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
